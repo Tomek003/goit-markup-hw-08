3af8960bb2e3b894086307150c9ef638a28d8b9a
Tomasz Niedoba <105716908+Tomek003@users.noreply.github.com>
2022-07-10 13:30:47 +0200

    Add files via upload

--- js/mobile.js
+++ js/mobile.js
@@ -1,8 +1,35 @@
-const mobileNav = document.querySelector('.mobile-menu');
-const burgerIcon = document.querySelector('.hamburger');
+const openMenuBtn = document.querySelector('.hamburger');
+const mobileMenu = document.querySelector('.mobile-menu');
+const closeMenuBtn = document.querySelector('.mobile-menu__close-btn');
 
-burgerIcon.addEventListener('click', function(){
-    mobileNav.classList.toggle('active');
-    burgerIcon.classList.toggle('active');
+const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+        ? 'disableBodyScroll'
+        : 'enableBodyScroll';
+      bodyScrollLock[scrollLockMethod](document.body);
+}
+
+openMenuBtn.addEventListener('click', () => {
+    openMenuBtn.classList.toggleMenu('active');
+    closeMenuBtn.classList.toggleMenu('active');
     
 })
+
+document.querySelectorAll('.mobile-menu__link').forEach(n => n.
+    addEventListener('click', () => {
+        openMenuBtn.classList.remove('active');
+        mobileMenu.classList.remove('active');
+    }))
+
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove('is-open');
+        openMenuBtn.setAttribute('aria-expanded', false);
+        bodyScrollLock.enableBodyScroll(document.body);
+      });
